79d1174272948d8824a0df4d6706fe3ada7c73e4
uchechukwu <uchechidi9@gmail.com>
2018-06-29 15:44:40 +0100

    MY FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,38 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+// Build table from form's input data
+function makeGrid() {
+    let w, h;
+    w = $('#input_width').val();
+    h = $('#input_height').val();
+    for(tr=1; tr<=h; tr++) { // Build table rows
+      $('#pixel_canvas').append('<tr></tr>');
+    }
+    for(td=1; td<=w; td++){ // Build table columns
+      $('tr').append('<td></td>');
+    }
+  }
+  
+  // Submit button
+  $('#sizePicker').submit(function(e) {
+    e.preventDefault();  //prevent form from submitting
+    let tr = $('#pixel_canvas').find('tr');
+    tr.remove();
+    makeGrid();
+  });
+  
+  // Aplying color to td's background
+  $('#pixel_canvas').on('click','td', function(e) {
+    let color = $('#colorPicker').val();
+    $(e.target).css('background-color', color);
+  });
+  
+  // Test alert
+  // $('#sizePicker').submit(function(e) {
+  //     e.preventDefault();  //prevent form from submitting
+  //     alert('Handler for .submit() called.' + makeGrid());
+  // });
+  
